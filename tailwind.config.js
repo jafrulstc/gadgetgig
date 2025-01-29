f4011614af815fc78494ad4df562e63b81392f56
jafrul-islam <jafrulstc@gmail.com>
2025-01-29 07:33:45 +0600

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "msm": { "max": "640px"},
+        "@2xl": { "max": "672px" },
+        "@xl": { "max": "576px" },
+        "@lg": { "max": "512px" },
+        "@md": { "max": "448px" },
+
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  
 };
